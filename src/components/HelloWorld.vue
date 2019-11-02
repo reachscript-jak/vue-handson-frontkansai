96d6fa47106c4b9c4b1996d680fccd1ef355a0e0
Takumi Okada <t_okada@freelance-jak.com>
2019-11-02 12:04:25 +0900

    ハンズオン終了

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <div>{{ propValue }}</div>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,7 +36,11 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+    propValue: {
+      type: Number,
+      default: 0,
+    },
   }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="home">
+    <div>{{ text }}</div>
+    <div>{{ num + (num * tax) }}</div>
+    <div>{{ flag ? 'OK' : 'NG' }}</div>
+    <div>{{howmuch}}</div>
+    <div v-show="vShowFlag">ここがvShow</div>
+    <div v-if="vIfFlag">ここがvIf</div>
+    <div v-for="(val, idx) in listItem" :key="idx">{{ val }}</div>
+    <input v-model="inputText" />
+    <div>{{ inputText }}</div>
+    <br>
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" :propValue="20" />
   </div>
 </template>
 
@@ -13,6 +23,33 @@ export default {
   name: 'home',
   components: {
     HelloWorld
-  }
+  },
+  data: function() {
+    return {
+      text: 'price',
+      num: 100,
+      tax: 0.10,
+      flag: true,
+      vShowFlag: true,
+      vIfFlag: false,
+      listItem: ['apple','banana','orange'],
+      inputText: '',
+    }
+  },
+  computed: {
+    howmuch() {
+      const { text, num, tax } = this;
+      return text + " is " + (num + (num * tax))
+    }
+  },
+  created() {
+    console.log('created!');
+  },
+  mounted() {
+    console.log('mounted!');
+  },
+  beforeDestroy() {
+    console.log('beforeDestroy');
+  },
 }
 </script>
